a965c87d2e9c09d9c7f6f15f4cb1609c305b5cab
Bolzbrain <moe.moe@mailbox.org>
2021-01-06 19:21:04 +0100

    Add files via upload

--- checker.sh
+++ checker.sh
@@ -2,13 +2,42 @@
 
 
 curl -s https://en.wiktionary.org/wiki/${1}#german >> ${1}.txt
-echo "noun:"
-cat ${1}.txt | grep -c German_noun
-echo "verb:"
-cat ${1}.txt | grep -c German_verb
-echo "adjective"
-cat ${1}.txt | grep -c German_adjective
-echo "pronoun"
-cat ${1}.txt | grep -c German_pronoun
+
+#echo "noun:"
+var1=$(cat ${1}.txt | grep -c German_noun)
+#echo ${var1} 
+
+#echo "verb:"
+var2=$(cat ${1}.txt | grep -c German_verb)
+#echo ${var2}
+
+#echo "adjective"
+var3=$(cat ${1}.txt | grep -c German_adjective)
+#echo ${var3}
+
+#echo "pronoun"
+var4=$(cat ${1}.txt | grep -c German_pronoun)
+#echo ${var4}
+
+
 rm ${1}.txt
 
+if [[ $var1 -gt 0 ]]
+then
+printf "es ist ein noun \n" 
+fi
+
+if [[ $var2 -gt 0 ]]
+then
+printf "es ist ein verb \n"
+fi
+
+if [[ $var3 -gt 0 ]]
+then
+printf "es ist ein adjective \n"
+fi
+
+if [[ $var4 -gt 0 ]]
+then
+printf "es ist ein pronoun \n"
+fi
