a502b2e416af9f31ef6f7fe048be2e1dfc84e3c9
Bolzbrain <moe.moe@mailbox.org>
2021-01-06 12:46:26 +0100

    Add files via upload

--- checker.sh
+++ checker.sh
@@ -2,9 +2,13 @@
 
 
 curl -s https://en.wiktionary.org/wiki/${1}#german >> ${1}.txt
-cat ${1}.txt | grep -c noun
-cat ${1}.txt | grep -c verb
-cat ${1}.txt | grep -c adjective
-cat ${1}.txt | grep -c pronoun
+echo "noun:"
+cat ${1}.txt | grep -c German_noun
+echo "verb:"
+cat ${1}.txt | grep -c German_verb
+echo "adjective"
+cat ${1}.txt | grep -c German_adjective
+echo "pronoun"
+cat ${1}.txt | grep -c German_pronoun
 rm ${1}.txt
 
